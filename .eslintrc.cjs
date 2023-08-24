71353819b0e72d9b99cbccab4d591644c16adac9
Alvin F <alvin@modern.kiwi>
2023-08-25 00:52:50 +1200

    Configured ESLint for Prettier+Tailwindcss.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,16 @@
 /** @type {import('eslint').Linter.Config} */
+
 module.exports = {
-	extends: ['@remix-run/eslint-config', '@remix-run/eslint-config/node'],
+	root: true,
+	extends: [
+		'@remix-run/eslint-config',
+		'@remix-run/eslint-config/node',
+		'plugin:tailwindcss/recommended',
+	],
+	overrides: [
+		{
+			files: ['*.ts', '*.tsx', '*.js'],
+			parser: '@typescript-eslint/parser',
+		},
+	],
 };
